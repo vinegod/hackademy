21cb2a516115d483ebcf2d44d86c8f8bcf047522
vinegod <denkalenichenko@gmail.com>
2021-12-26 18:22:43 +0200

    nice

---
+++ courses/golang/ex05-parallel/parallel_letter_frequency.go
@@ -0,0 +1,32 @@
+package letter
+
+func Frequency(letter string) map[rune]int {
+	characters := make(map[rune]int)
+
+	for _, r := range letter {
+		characters[r]++
+	}
+	return characters
+}
+
+func frequency(letter string, mp chan map[rune]int) {
+	mp <- Frequency(letter)
+}
+
+func ConcurrentFrequency(letters []string) map[rune]int {
+	maps := make(chan map[rune]int)
+
+	for i := range letters {
+		go frequency(letters[i], maps)
+	}
+
+	characters := make(map[rune]int)
+
+	for i := 0; i < len(letters); i++ {
+		for n, r := range <-maps {
+			characters[n] += r
+		}
+	}
+
+	return characters
+}
